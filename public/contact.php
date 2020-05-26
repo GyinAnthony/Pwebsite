bf0e651fc1bff2e4b4ca43ab227f81e340cf6882
Anthony Gao <gaoyinanthony@gmail.com>
2020-05-26 01:17:44 -0500

    Finished Footer and added some css

--- public/contact.php
+++ public/contact.php
@@ -9,16 +9,15 @@ $subject = 'Form Submission';
 $message = "Name: " .$name. "\n". "email: " .$email. "\n". "Message: " .$msg;
 $headers= "From: " .$email;
 
-if (empty($name) || empty($email) || empty($msg))
+if(mail($to,$subject,$message,$headers))
 {
-    echo "Please fill all the fields";
+        echo "<h1> Sent Sucessfully! Thank you"." ".$name.", I will contact you shortly";
 }
 else
 {
-    if(mail($to,$subject,$message,$headers))
-    {
-        echo "<h1> Sent Sucessfully! Thank you"." ".$name.", I will contact you shortly";
-    }
+    echo "<script type = 'text/javascript'>
+    alert('There is an issue please try again later')
+    </script>";
 
 
 
